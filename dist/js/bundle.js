e8ba4701c039b1f8e7b9739e496a9ac171a960d1
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2024-11-21 14:19:40 +0300

    responsive calc

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -191,9 +191,10 @@ function validateRequired() {
 
 $(document).ready(function () {
 
-  $('.modal-backdrop').on("click", function (e) {
+  $(document).on("click", '.modal-backdrop', function (e) {
     $('.modal').modal('hide')
   })
+
 
   
   $("[data-modaltab]").on("click", function (e) {
